138819308e0ed0d859e43a2b1d774011af8950c1
Stefanie Sarközi <sarkoezi.stefanie@gmail.com>
2023-05-31 11:30:19 +0200

    finish basic exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+let age = "25";
+let name = "Steffi";
+
+console.log(`Hello my name is ${name} and I\'m ${age} years old.`);
+
+let players=["Martin", "Thomas", "Peter", "Mathias", "Niki"]
+
+console.log(players)
+
